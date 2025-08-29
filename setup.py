15b056880448dbfaa236258bdbe5b4931202a5b1
Bipin Dahat <bipindahat857@gmail.com>
2025-08-29 20:21:38 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+setup(
+    name='Medical-Assistant',
+    version='0.0.0',
+    author='Bipin Dahat',
+    author_email='bipindahat857@gmail.com',
+    packages=find_packages(),
+    install_requires =[]
+
+)
